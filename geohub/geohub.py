e2743f09b90b77f5258dbab131e08d71d40ef1d9
Mahnaz Meem <mahnazsarkermeem@gmail.com>
2024-03-06 14:41:48 -0500

    test

--- geohub/geohub.py
+++ geohub/geohub.py
@@ -6,3 +6,14 @@ class Map(ipyleaflet.Map):
         super().__init__(center=center, zoom=zoom, **kwargs)
         self.add_control(ipyleaflet.LayersControl())
 
+        def add_tile_layer(self, url, name, attribution):
+            tile_layer = ipyleaflet.TileLayer(url=url, name=name, attribution=attribution)
+            self.add_layer(tile_layer)
+
+        def add_basemap(self, name):
+            if isinstance(name, str):
+                basemap = eval(f"basemaps.{name}")
+                self.add(basemap)
+            else:
+                self.add(name)
+
